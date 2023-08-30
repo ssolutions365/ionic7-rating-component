d358985cd03584983d063725e903b30f2b55a23e
Jefferson Quintero <quinterpriest@gmail.com>
2023-08-30 11:12:36 -0500

    ajustes iniciales

---
+++ src/lib/ionic-rating.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IonicRatingService {
+
+  private starRatingSubject = new Subject<number>();
+  private topicSubject = new Subject<{ [key: string]: any }>();
+
+  publishStartRatingChanged(value: number) {
+    this.starRatingSubject.next(value);
+  }
+
+  getStartRatingChangedObservable(): Subject<any> {
+    return this.starRatingSubject;
+  }
+
+  publishTopic(topic: string, value: number) {
+    this.topicSubject.next({ topic, value });
+  }
+
+  getTopicObservable(key): Subject<any> {
+    return this.topicSubject[key];
+  }
+}
